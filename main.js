e78d193fc679aef8a35573eabfb5300b0efe3c6a
Giang Nam <namle@kinssolutions.net>
2018-03-09 19:48:11 +0700

    asd

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-server.listen(8086, function() {
+app.listen(8086, function() {
     console.log('Example app listening on port 8086!');
 });
