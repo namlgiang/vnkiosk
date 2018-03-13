73d845d21c12b70db65315cf6653e4849672672a
Giang Nam <namle@kinssolutions.net>
2018-03-13 15:37:03 +0700

    sd

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
+var sqlite = require('sqlite3');
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/subscribe", function(req, res) {
+    console.log(req.body);
+    res.end();
+});
 
 app.listen(8086, function() {
     console.log('Example app listening on port 8086!');
--- public/main.js
+++ public/main.js
@@ -10,4 +10,25 @@ $(document).ready(function() {
     $(".hotline, .section1 .order").click(function() {
         $("body, html").animate({scrollTop: $(".section4").offset().top}, 500);
     });
+
+    $(".section4 .order").click(function() {
+        var inputs = $(".section4 input");
+        var valid = true;
+        for(var i=0; i<inputs.length; i++)
+            if($(inputs[i]).val().trim() == "") {
+                valid = false;
+                $(inputs[i]).addClass("error");
+            } else {
+                $(inputs[i]).removeClass("error");
+            }
+        if(!valid) return;
+
+        var data = {
+            first: $(".section4 .first").val(),
+            last: $(".section4 .last").val(),
+            email: $(".section4 .email").val(),
+            phone: $(".section4 .phone").val(),
+            place: $(".section4 .place").val()
+        };
+    });
 });
